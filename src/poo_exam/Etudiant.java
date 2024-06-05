364ddc56fabc299a8994fbf79f2755288d400961
Thommy <you@example.com>
2024-06-05 11:47:14 +0200

    initial commigt

---
+++ src/poo_exam/Etudiant.java
@@ -0,0 +1,7 @@
+package poo_exam;
+
+public abstract class Etudiant extends Individu{
+	private Date dateInscription;
+	private String niveauEtude;
+	
+}
---
+++ src/poo_exam/Individu.java
@@ -0,0 +1,28 @@
+package poo_exam;
+
+public abstract class Individu {
+	private String nom;
+    private String prenom;
+    private int identifiant;
+	
+public int getIdentifiant() {
+		return identifiant;
+	}
+	public void setIdentifiant(int identifiant) {
+		this.identifiant = identifiant;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+public abstract void montrerDetails();  
+    
+}
